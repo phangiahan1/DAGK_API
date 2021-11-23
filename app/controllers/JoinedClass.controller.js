4b1cf796e96e6cdce500f3a8492a46c68f58fde4
phangiahan1 <phanhan226@gmail.com>
2021-11-23 13:23:46 +0700

    fix joined class bi trung

--- app/controllers/JoinedClass.controller.js
+++ app/controllers/JoinedClass.controller.js
@@ -1,6 +1,6 @@
 const JoinedClass = require('../models/JoinedClass.model.js');
 const UserController = require('../controllers/user.controller');
-const User = require('../models/User.model.js');
+const User = require('../models/user.model.js');
 
 // Retrieve and return all Classroom from the database.
 exports.findAllbyClassId = (req, res) => {
@@ -113,23 +113,35 @@ exports.inviteTeacher = (req, res) => {
         .then(user => {
             if (!user) {
                 return res.status(404).send({
-                    message: "User not found with email " + req.params.email
+                    message: "User not found with email " + req.body.email
                 });
             }
-            const joinedNew = new JoinedClass({
-                idClass: req.params.id,
-                idUser: user[0]._id,
-                type: true, //type true: teacher
-                hide: false
-            });
-            joinedNew.save()
-                .then(data => {
-                    res.send(data);
-                }).catch(err => {
-                    res.status(500).send({
-                        message: err.message || "Some error occurred while creating the Classroom."
-                    });
-                });
+            else {
+                JoinedClass.find({ idClass: req.params.id, idUser: user[0]._id })
+                    .then(data => {
+                        if (data) {
+                            return res.status(404).send({
+                                message: "User in class: " + data
+                            });
+                        }
+                        else {
+                            const joinedNew = new JoinedClass({
+                                idClass: req.params.id,
+                                idUser: user[0]._id,
+                                type: true, //type true: teacher
+                                hide: false
+                            });
+                            joinedNew.save()
+                                .then(data => {
+                                    res.send(data);
+                                }).catch(err => {
+                                    res.status(500).send({
+                                        message: err.message || "Some error occurred while creating the Classroom."
+                                    });
+                                });
+                        }
+                    })
+            }
         })
 };
 
@@ -142,19 +154,31 @@ exports.inviteStudent = (req, res) => {
                     message: "User not found with email " + req.params.email
                 });
             }
-            const joinedNew = new JoinedClass({
-                idClass: req.params.id,
-                idUser: user[0]._id,
-                type: false, //type false stu
-                hide: false
-            });
-            joinedNew.save()
-                .then(data => {
-                    res.send(data);
-                }).catch(err => {
-                    res.status(500).send({
-                        message: err.message || "Some error occurred while creating the Classroom."
-                    });
-                });
+            else {
+                JoinedClass.find({ idClass: req.params.id, idUser: user[0]._id })
+                    .then(data => {
+                        if (data) {
+                            return res.status(404).send({
+                                message: "User in class: " + data
+                            });
+                        }
+                        else {
+                            const joinedNew = new JoinedClass({
+                                idClass: req.params.id,
+                                idUser: user[0]._id,
+                                type: false, //type false stu
+                                hide: false
+                            });
+                            joinedNew.save()
+                                .then(data => {
+                                    res.send(data);
+                                }).catch(err => {
+                                    res.status(500).send({
+                                        message: err.message || "Some error occurred while creating the Classroom."
+                                    });
+                                });
+                        }
+                    })
+            }
         })
 };
